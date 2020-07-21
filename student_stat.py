9046044534252fb6c7fcb97287414f1ad68dab81
gmlove <gracekinglau@gmail.com>
2020-07-21 14:06:10 +0800

    basic spark application

---
+++ student_stat.py
@@ -0,0 +1,12 @@
+
+
+class GradeStat:
+
+    def __init__(self, well_count, less_well_count, not_good_count):
+        self.well_count = well_count
+        self.less_well_count = less_well_count
+        self.not_good_count = not_good_count
+
+
+def student_grade_stat(data_file: str) -> GradeStat:
+    pass
